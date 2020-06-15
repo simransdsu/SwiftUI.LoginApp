a272b323cb9ee85d64fba8b2f15ff016cbd0a166
Simran <simran5590@gmail.com>
2020-06-14 20:42:47 -0700

    Login UI with custom components

--- LoginApp/ContentView.swift
+++ LoginApp/ContentView.swift
@@ -9,13 +9,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var username = ""
+    @State var password = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            WelcomeText()
+            UserImage()
+            UsernameTextField(username: $username)
+            PasswordTextField(password: $password)
+            
+            Text("LOGIN")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 220, height: 60)
+                .background(Color.green)
+                .cornerRadius(15)
+                .onTapGesture {
+                    print("Login tapped.")
+                }
+        }.padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct PasswordTextField: View {
+    
+    @Binding var password: String
+    
+    var body: some View {
+        SecureField("Password", text: $password)
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(10)
+    }
+}
+
+struct UsernameTextField: View {
+    
+    @Binding var username: String
+    
+    var body: some View {
+        SecureField("Username", text: $username)
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(10)
+    }
+}
+
+
+struct WelcomeText: View {
+    var body: some View {
+        Text("Welcome!")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .padding(.bottom, 20)
+    }
+}
+
+struct UserImage: View {
+    var body: some View {
+        Image("userImage")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 150, height: 150)
+            .clipped()
+            .cornerRadius(150)
+            .padding(.bottom, 75)
     }
 }
---
+++ LoginApp/Utils.swift
@@ -0,0 +1,6 @@
+import Foundation
+import SwiftUI
+
+let lightGreyColor = Color(red: 239.0/255.0,
+green: 243.0/255.0, blue: 244.0/255.0,
+opacity: 1.0)
